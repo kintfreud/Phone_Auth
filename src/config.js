7714a5753ae04416746cb25f801296faa7c43850
kintfreud <81480655+kintfreud@users.noreply.github.com>
2022-11-03 17:40:38 +0000

    first add

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    serviceID : 'VAa9b9bf95613f250d44dee88f846bad3b',
+    accountSID : 'AC6516ebfb64999bfa9f140b872db8da04',
+    authToken : 'e52641933124482c3e8cc43b440acb56'
+};
