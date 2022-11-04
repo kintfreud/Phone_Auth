cbce707aa838dd41ecd27c07be4988864374b9d5
kintfreud <81480655+kintfreud@users.noreply.github.com>
2022-11-04 11:11:59 +0000

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    serviceID : 'VAd42df0de247b88c5975b9453470e95e4',
+   // 'VAa9b9bf95613f250d44dee88f846bad3b',
+    accountSID : 'AC0a4149e30f1da33fbfa8599d62c2bd99',
+  //  'AC6516ebfb64999bfa9f140b872db8da04',
+    authToken : 'eaa62b3c7d6d4aefcb9d82f45a61ee07',
+  //  'e52641933124482c3e8cc43b440acb56'
+};
