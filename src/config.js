5102a4f81c3ceb89fe9e7c0bb20e9366ac0c1f53
kintfreud <81480655+kintfreud@users.noreply.github.com>
2022-11-04 11:11:11 +0000

    Delete config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    serviceID : 'VAa9b9bf95613f250d44dee88f846bad3b',
-    accountSID : 'AC6516ebfb64999bfa9f140b872db8da04',
-    authToken : 'e52641933124482c3e8cc43b440acb56'
-};
